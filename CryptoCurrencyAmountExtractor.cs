b3102cab1916d6b7638c098b344fdb5dff6acb1f
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2017-11-30 23:20:31 +0100

    Inital commit, just got the bot up

---
+++ CryptoCurrencyAmountExtractor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace DashTipper
+{
+	public class CryptoCurrencyAmountExtractor
+	{
+		public static bool TryExtract(string input, out decimal amountInMdash)
+		{
+			var amountWithSymbol = TryExtractCoinAmountWithSymbol(input);
+			if (amountWithSymbol == null)
+			{
+				amountInMdash = default(decimal);
+				return false;
+			}
+			amountInMdash = ConvertToMdash(amountWithSymbol);
+			return true;
+		}
+
+		private static CoinAmountWithSymbol TryExtractCoinAmountWithSymbol(string input)
+		{
+			var matchCollection = CoinRegex.Matches(input);
+			if (matchCollection.Count == 1)
+				return TryExtractCoinAmountWithSymbol(matchCollection[0].Groups);
+			return null;
+		}
+
+		private static readonly Regex CoinRegex = new Regex(CoinMatchPattern, CoinMatchOptions);
+		private const string CoinMatchPattern =
+			@"([-+]?[0-9]*\.?[0-9]+)\s?(dash|mdash|udash|duff)";
+		private const RegexOptions CoinMatchOptions =
+			RegexOptions.IgnoreCase | RegexOptions.CultureInvariant;
+
+		private class CoinAmountWithSymbol
+		{
+			public CoinAmountWithSymbol(decimal amount, string symbol)
+			{
+				Amount = amount;
+				Symbol = symbol;
+			}
+
+			public decimal Amount { get; }
+			public string Symbol { get; }
+		}
+
+		private static CoinAmountWithSymbol TryExtractCoinAmountWithSymbol(
+			GroupCollection matchedGroups)
+		{
+			string amountAsString = matchedGroups[1].Value;
+			string symbol = matchedGroups[2].Value;
+			decimal amount = decimal.Parse(amountAsString, CultureInfo.InvariantCulture);
+			return new CoinAmountWithSymbol(amount, symbol);
+		}
+
+		private static decimal ConvertToMdash(CoinAmountWithSymbol amountWithSymbol)
+		{
+			decimal amount = amountWithSymbol.Amount;
+			switch (amountWithSymbol.Symbol.ToLowerInvariant())
+			{
+			case "dash":
+				return amount * 1000m;
+			case "mdash":
+				return amount;
+			case "udash":
+				return amount * 0.001m;
+			case "duff":
+				return amount * 0.00001m;
+			default:
+				throw new Exception($"Unsupported symbol: '{amountWithSymbol.Symbol}'");
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DashTipper
+{
+	class Program
+	{
+		static void Main()
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+			client.Log += Log;
+			client.MessageReceived += MessageReceived;
+			string token = "-privatetoken-";
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+		private DiscordSocketClient client;
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		private static async Task MessageReceived(SocketMessage message)
+		{
+			if (message.Content.StartsWith("!tip"))
+				await HandleTip(message);
+		}
+
+		private static async Task HandleTip(SocketMessage message)
+		{
+			var tipUser = message.MentionedUsers.FirstOrDefault();
+			if (tipUser == null || !CryptoCurrencyAmountExtractor.TryExtract(message.Content,
+				out decimal amountInMdash))
+				await message.Channel.SendMessageAsync(
+					"Please specify a username and amount (DASH, mDASH, uDASH, Duff), e.g. " +
+					"@TheDesertLynx !tip 1mDASH");
+			else
+				await message.Channel.SendMessageAsync("TODO: Successfully tipped @" + tipUser.Username +
+					" " + amountInMdash + " mDASH");
+		}
+	}
+}
